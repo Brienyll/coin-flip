071ecda1ac9907fc000f965a776c9cac1c3fd327
Brienyll <Brienyll29@gmail.com>
2019-06-22 03:34:07 -0700

    added images

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { choice } from './helper';
+import Coin from './Coin';
 
 class CoinContainer extends Component {
   static defaultProps = {
     coins: [
-      {side: 'heads', imgSrc:"https://tinyurl.com/react-coin-heads-jpg"},
-      {side: 'tails', imgSrc:"https://tinyurl.com/react-coin-tails-jpg"}
+      {side: 'heads', imgSrc:"https://random-ize.com/coin-flip/us-quarter/us-quarter-front.jpg"},
+      {side: 'tails', imgSrc:"https://random-ize.com/coin-flip/us-quarter/us-quarter-back.jpg"}
     ]
   };
   constructor(props) {
@@ -38,7 +39,9 @@ class CoinContainer extends Component {
     return (
       <div className="CoinContainer">
         <h2> Flip A Coin</h2>
+        {this.state.currCoin && <Coin info={this.state.currCoin}/>}
         <button onClick={this.handleClick}>Flip Coin!</button>
+        
         <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} Heads 
         and {this.state.nTails} Tails.</p>
       </div>
